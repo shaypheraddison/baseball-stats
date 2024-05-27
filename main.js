996b04615c09665f69a02ac65c51a366dda9a0fd
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-05-27 02:44:54 -0500

    completed functionality of doing the math for hitting stats

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 
 const inputData = document.getElementById("stat-input");
 const playerChoice = document.getElementById("player-choice");
-const nameButton = document.getElementById("name-button");
+const mainButton = document.getElementById("main-button");
 const baseballStatsDisplay = document.getElementById("baseball-stats");
 
 const atBats = document.getElementById("at-bats");
@@ -44,26 +44,76 @@ const defaultBackground = "Resources/baseball.jpeg";
 function selectPlayer() {
     const batterStats = document.getElementById("batter-stats");
     const pitcherStats = document.getElementById("pitcher-stats");
+    const nameContainer = document.querySelector(".input-data");
 
     playerChoice.addEventListener("change", function(event) {
         const playerValue = event.target.value;
         if (playerValue=== "batter") {
-            batterStats.style.display = "inline";
+            batterStats.style.display = "block";
             pitcherStats.style.display = "none";
+            nameContainer.style.display = "flex";
             document.body.style.backgroundImage = `url(${batterBackground})`;
         } else if (playerValue === "pitcher") {
-            pitcherStats.style.display = "inline";
+            pitcherStats.style.display = "block";
             batterStats.style.display = "none";
+            nameContainer.style.display = "flex";
             document.body.style.backgroundImage = `url(${pitcherBackground})`;
         } else {
             pitcherStats.style.display = "none";
             batterStats.style.display = "none";
+            nameContainer.style.display = "none";
             document.body.style.backgroundImage = `url(${defaultBackground})`;
-        };
+        }
     });
 };
 
+function calcBattingAverage(hitsStat, atBatsStat) {
+    const average = Number(hitsStat) / Number(atBatsStat);
+    const roundedAverage = average.toFixed(3);
+    return roundedAverage
+};
+
+function calcOnBasePercentage(hitsStat, atBatsStat, walksStat, hitByPitchesStat, sacrificeFliesStat) {
+    const denominator = Number(atBatsStat) + Number(walksStat) + Number(sacrificeFliesStat) + Number(hitByPitchesStat);
+    const onBaseNumerator = Number(hitsStat) + Number(walksStat) + Number(hitByPitchesStat);
+    const onBase = onBaseNumerator / denominator;
+    const roundedOnBase = onBase.toFixed(3);
+    return roundedOnBase;
+};
+
+function calcSlugging(singlesStat, doublesStat, triplesStat, homeRunsStat, atBatsStat) {
+    const numerator = (Number(singlesStat) + (2 * Number(doublesStat))+ (3 * Number(triplesStat)) + (4 * Number(homeRunsStat)));
+    const slugging = numerator / Number(atBatsStat);
+    const roundedSlugging = slugging.toFixed(3);
+    return roundedSlugging
+};
+
+function calcOnBasePlusSlugging(hitsStat, atBatsStat, walksStat, hitByPitchesStat, sacrificeFliesStat, singlesStat, doublesStat, triplesStat, homeRunsStat) {
+    const obpStat = parseFloat(calcOnBasePercentage(hitsStat, atBatsStat, walksStat, hitByPitchesStat, sacrificeFliesStat));
+    const sluggingStat = parseFloat(calcSlugging(singlesStat, doublesStat, triplesStat, homeRunsStat, atBatsStat));
+    const opsStat = obpStat + sluggingStat;
+    const roundedOPS = opsStat.toFixed(3);
+    return roundedOPS;
+};
 
 
+function hittingMath() {
+    const battingAverage = calcBattingAverage(hits.value, atBats.value);
+    const onBasePercentage = calcOnBasePercentage(hits.value, atBats.value, walks.value, sacrificeFlies.value, hitByPitches.value);
+    const slugging = calcSlugging(singles.value, doubles.value, triples.value, homeRuns.value, atBats.value);
+    const onBasePlusSlugging = calcOnBasePlusSlugging(hits.value, atBats.value, walks.value, sacrificeFlies.value, hitByPitches.value, singles.value, doubles.value, triples.value, homeRuns.value);
 
-selectPlayer();
+    console.log(`Slash Line is: ${battingAverage}/${onBasePercentage}/${slugging}/${onBasePlusSlugging}`);
+    return { battingAverage, onBasePercentage, slugging, onBasePlusSlugging }
+};
+
+
+function main() {
+    selectPlayer();
+    mainButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        hittingMath();
+    });  
+};
+
+main();
